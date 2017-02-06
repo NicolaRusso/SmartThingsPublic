ef7a772a50777dc35231a218e35e656a7af211cc
Nicola <dott.nicolarusso@gmail.com>
2017-02-06 15:12:08 +0000

    new devices + bug fix
    Lights (control, color, kelvin) & Outlet (control, meter) support , Dim capability, Temp Sensor bug fix (scale).

--- smartapps/gideon-api/gideon.src/gideon.groovy
+++ smartapps/gideon-api/gideon.src/gideon.groovy
@@ -24,10 +24,21 @@ definition(
     iconX3Url: "http://s33.postimg.org/t77u7y7v3/logo.png",
     oauth: [displayName: "Gideon Smart Home API app", displayLink: "gideon.ai"])
 
-
 preferences {
 	section("Control these contact sensors...") {
         input "contact", "capability.contactSensor", multiple:true, required:false
+    }
+    section("Control these switch levels...") {
+        input "switchlevels", "capability.switchLevel", multiple:true, required:false
+    }
+/*    section("Control these thermostats...") {
+        input "thermostats", "capability.thermostat", multiple:true, required:false
+    }*/
+    section("Control the color for these devices...") {
+        input "colors", "capability.colorControl", multiple:true, required:false
+    }
+    section("Control the color temperature for these devices...") {
+        input "kelvin", "capability.colorTemperature", multiple:true, required:false
     }
 	section("Control these switches...") {
         input "switches", "capability.switch", multiple:true, required:false
@@ -50,11 +61,10 @@ preferences {
     section("Control these presence sensors...") {
     	input "presence_sensors", "capability.presenceSensor", multiple:true, required:false
     }
-/** section("Control these outlets...") {
-    	input "outlets", "capability.switch", multiple:true, required:false
+	section("Control these outlets...") {
+    	input "outlets", "capability.outlet", multiple:true, required:false
     }
-*/
-     section("Control these power meters...") {
+    section("Control these power meters...") {
         input "meters", "capability.powerMeter", multiple:true, required:false
     }
     section("Control these locks...") {
@@ -95,6 +105,63 @@ mappings {
       	GET: "getAllDevices"
    ]
   }
+  /*
+  path("/thermostat/setcool/:id/:temp") {
+    action: [
+      GET: "setCoolTemp"
+    ]
+  }
+  path("/thermostat/setheat/:id/:temp") {
+    action: [
+      GET: "setHeatTemp"
+    ]
+  }
+  path("/thermostat/setfanmode/:id/:mode") {
+    action: [
+      GET: "setFanMode"
+    ]
+  }
+  path("/thermostat/setmode/:id/:mode") {
+    action: [
+      GET: "setThermostatMode"
+    ]
+  }
+  path("/thermostat/:id") {
+    action: [
+      GET: "getThermostatStatus"
+    ]
+  }
+  */
+  path("/light/dim/:id/:dim") {
+    action: [
+      GET: "setLevelStatus"
+    ]
+  }
+  path("/light/kelvin/:id/:kelvin") {
+    action: [
+      GET: "setKelvin"
+    ]
+  }
+  path("/colorlight/:id/:hue/:sat") {
+    action: [
+      GET: "setColor"
+    ]
+  }
+  path("/light/status/:id") {
+    action: [
+      GET: "getLightStatus"
+    ]
+  }
+  path("/light/on/:id") {
+    action: [
+      GET: "turnOnLight"
+    ]
+  }
+  path("/light/off/:id") {
+    action: [
+      GET: "turnOffLight"
+    ]
+  }
   path("/doorlocks/lock/:id") {
     action: [
       GET: "lockDoorLock"
@@ -117,9 +184,9 @@ mappings {
   }
   path("/smoke/:id") {
     action: [
-      			GET: "getSmokeStatus"
-    		]
-  	}
+   	GET: "getSmokeStatus"
+	]
+  }
     path("/shades/open/:id") {
     action: [
       GET: "openShade"
@@ -137,9 +204,9 @@ mappings {
   }
   	path("/shades/:id") {
     action: [
-      			GET: "getShadeStatus"
-    		]
-  	}
+	GET: "getShadeStatus"
+	]
+}
     path("/garage/open/:id") {
     action: [
       GET: "openGarage"
@@ -152,14 +219,14 @@ mappings {
   }
   	path("/garage/:id") {
     action: [
-      			GET: "getGarageStatus"
-    		]
-  	}
+  		GET: "getGarageStatus"
+  	]
+  }
     path("/watersensors/:id") {
     action: [
-      			GET: "getWaterSensorStatus"
-    		]
-  	}
+      		GET: "getWaterSensorStatus"
+    	]
+  }
   	path("/tempsensors/:id") {
     action: [
       GET: "getTempSensorsStatus"
@@ -185,7 +252,7 @@ mappings {
       GET: "getMotionStatus"
     ]
   }
-/**  	path("/outlets/:id") {
+  	path("/outlets/:id") {
     action: [
       GET: "getOutletStatus"
     ]
@@ -200,8 +267,7 @@ mappings {
       GET: "turnOffOutlet"
     ]
   }
-*/
-  	path("/switches/turnon/:id") {
+  path("/switches/turnon/:id") {
     action: [
       GET: "turnOnSwitch"
     ]
@@ -220,7 +286,10 @@ mappings {
 
 //API Methods
 def getAllDevices() {
-	def locks_list = locks.collect{device(it,"Lock")}
+    def locks_list = locks.collect{device(it,"Lock")}
+    /*def thermo_list = thermostats.collect{device(it,"Thermostat")}*/
+    def colors_list = colors.collect{device(it,"Color")}
+    def kelvin_list = kelvin.collect{device(it,"Kelvin")}
     def contact_list = contact.collect{device(it,"Contact Sensor")}
     def smokes_list = smoke_alarms.collect{device(it,"Smoke Alarm")}
     def shades_list = shades.collect{device(it,"Window Shade")}
@@ -228,13 +297,196 @@ def getAllDevices() {
     def water_sensors_list = water_sensors.collect{device(it,"Water Sensor")}
     def presences_list = presence_sensors.collect{device(it,"Presence")}
     def motions_list = motions.collect{device(it,"Motion")}
-  /**  def outlets_list = outlets.collect{device(it,"Outlet")} */
+	def outlets_list = outlets.collect{device(it,"Outlet")}
     def switches_list = switches.collect{device(it,"Switch")}
+    def switchlevels_list = switchlevels.collect{device(it,"Switch Level")}
     def temp_list = temperature_sensors.collect{device(it,"Temperature")}
     def meters_list = meters.collect{device(it,"Power Meters")}
     def battery_list = batteries.collect{device(it,"Batteries")}
-    return smokes_list + contact_list + water_sensors_list + shades_list + garage_list + locks_list + presences_list + motions_list + switches_list + temp_list + meters_list + battery_list
+    return outlets_list + kelvin_list + colors_list + switchlevels_list + smokes_list + contact_list + water_sensors_list + shades_list + garage_list + locks_list + presences_list + motions_list + switches_list + temp_list + meters_list + battery_list
 }
+
+//thermostat
+/*
+def setCoolTemp() {
+	def device = thermostats.find { it.id == params.id }
+        if (!device) {
+            httpError(404, "Device not found")
+        } else {
+            if(device.hasCommand("setCoolingSetpoint")) {
+            	device.setCoolingSetpoint(params.temp.toInteger());
+                return [result_action: "200"]
+            }
+            else {
+            	httpError(510, "Not supported!")
+            }
+       }
+}
+
+def setHeatTemp() {
+	def device = thermostats.find { it.id == params.id }
+        if (!device) {
+            httpError(404, "Device not found")
+        } else {
+            if(device.hasCommand("setHeatingSetpoint")) {
+            	device.setHeatingSetpoint(params.temp.toInteger());
+                return [result_action: "200"]
+            }
+            else {
+            	httpError(510, "Not supported!")
+            }
+       }
+}
+
+def setFanMode() {
+	def device = thermostats.find { it.id == params.id }
+        if (!device) {
+            httpError(404, "Device not found")
+        } else {
+        if(device.hasCommand("setThermostatFanMode")) {
+            	device.setThermostatFanMode(params.mode);
+                return [result_action: "200"]
+            }
+            else {
+            	httpError(510, "Not supported!")
+            }
+       }
+}
+
+def setThermostatMode() {
+	def device = thermostats.find { it.id == params.id }
+        if (!device) {
+            httpError(404, "Device not found")
+        } else {
+        if(device.hasCommand("setThermostatMode")) {
+            	device.setThermostatMode(params.mode);
+                return [result_action: "200"]
+            }
+            else {
+            	httpError(510, "Not supported!")
+            }
+       }
+}
+
+def getThermostatStatus() {
+	def device = thermostats.find{ it.id == params.id }
+    if (!device) {
+            httpError(404, "Device not found")
+        } else {
+        	return [ThermostatOperatingState: device.currentValue('thermostatOperatingState'), ThermostatSetpoint: device.currentValue('thermostatSetpoint'), 
+            			ThermostatFanMode: device.currentValue('thermostatFanMode'), ThermostatMode: device.currentValue('thermostatMode')]
+       	}
+}
+*/
+//light
+def turnOnLight() {
+    def device = switches.find { it.id == params.id }
+        if (!device) {
+            httpError(404, "Device not found")
+        } else {
+            device.on();
+                  
+            return [Device_id: params.id, result_action: "200"]
+        }
+    }
+
+def turnOffLight() {
+    def device = switches.find { it.id == params.id }
+        if (!device) {
+            httpError(404, "Device not found")
+        } else {
+            device.off();
+                  
+            return [Device_id: params.id, result_action: "200"]
+        }
+}
+
+def getLightStatus() {
+	def device = switches.find{ it.id == params.id }
+    if (!device) {
+            httpError(404, "Device not found")
+        } else {
+        	return [Status: device.currentValue('switch'), Dim: getLevelStatus(params.id), Color: getColorStatus(params.id), Kelvin: getKelvinStatus(params.id)]
+        }
+}
+
+//color control
+def setColor() {
+    def device = colors.find { it.id == params.id }
+        if (!device) {
+            httpError(404, "Device not found")
+        } else {
+        	
+            def map = [hue:params.hue.toInteger(), saturation:params.sat.toInteger()]
+            
+            device.setColor(map);
+                  
+            return [Device_id: params.id, result_action: "200"]
+    }
+}
+
+def getColorStatus(id) {
+	def device = colors.find { it.id == id }
+    if (!device) {
+            return [Color: "none"]
+        } else {
+        	return [hue: device.currentValue('hue'), saturation: device.currentValue('saturation')]
+        }
+}
+
+//kelvin control
+def setKelvin() {
+    def device = kelvin.find { it.id == params.id }
+        if (!device) {
+            httpError(404, "Device not found")
+        } else {
+        
+            device.setColorTemperature(params.kelvin.toInteger());
+                  
+            return [Device_id: params.id, result_action: "200"]
+    }
+}
+
+def getKelvinStatus(id) {
+	def device = kelvin.find { it.id == id }
+    if (!device) {
+            return [kelvin: "none"]
+        } else {
+        	return [kelvin: device.currentValue('colorTemperature')]
+        }
+}
+
+//switch level
+def getLevelStatus() {
+	def device = switchlevels.find { it.id == params.id }
+    if (!device) {
+            [Level: "No dimmer"]
+        } else {
+        	return [Level: device.currentValue('level')]
+        }
+}
+
+def getLevelStatus(id) {
+	def device = switchlevels.find { it.id == id }
+    if (!device) {
+            [Level: "No dimmer"]
+        } else {
+        	return [Level: device.currentValue('level')]
+        }
+}
+
+
+def setLevelStatus() {
+	def device = switchlevels.find { it.id == params.id }
+    def level = params.dim
+    if (!device) {
+            httpError(404, "Device not found")
+        } else {
+        	device.setLevel(level.toInteger())
+        	return [result_action: "200", Level: device.currentValue('level')]
+        }
+}
+
 
 //contact sensors
 def getContactStatus() {
@@ -425,18 +677,20 @@ def getMotionStatus() {
 }
 
 //OUTLET
-/**
 def getOutletStatus() {
 	
     def device = outlets.find { it.id == params.id }
-   	if (!device) {
-            httpError(404, "Device not found")
-        } else {
-        	def watt = getMeterStatus(device.id)
-        	return [Device_state: device.currentSwitch] + watt
-  }
+    if (!device) {
+            device = switches.find { it.id == params.id }
+            if(!device) {
+            	httpError(404, "Device not found")
+        	}
+     }
+     def watt = getMeterStatus(device.id)
+       	
+     return [Device_state: device.currentValue('switch')] + watt
 }
-*/
+
 def getMeterStatus() {
 	
     def device = meters.find { it.id == params.id }
@@ -457,48 +711,47 @@ def getMeterStatus(id) {
   }
 }
 
-/**
+
 def turnOnOutlet() {
     def device = outlets.find { it.id == params.id }
-    if (command){
-        if (!device) {
-            httpError(404, "Device not found")
-        } else {
-            
-            device.on();
+    if (!device) {
+            device = switches.find { it.id == params.id }
+            if(!device) {
+            	httpError(404, "Device not found")
+        	}
+     }
+     
+     device.on();
                   
-            return [Device_id: params.id, result_action: "200"]
-        }
-    }
+     return [Device_id: params.id, result_action: "200"]
 }
 
 def turnOffOutlet() {
     def device = outlets.find { it.id == params.id }
-    if (command){
-        if (!device) {
-            httpError(404, "Device not found")
-        } else {
+    if (!device) {
+            device = switches.find { it.id == params.id }
+            if(!device) {
+            	httpError(404, "Device not found")
+        	}
+     }
             
-            device.off();
+     device.off();
                   
-            return [Device_id: params.id, result_action: "200"]
-        }
-    }
+     return [Device_id: params.id, result_action: "200"]
 }
-*/
+
 //SWITCH
 def getSwitchStatus() {
 	def device = switches.find { it.id == params.id }
     if (!device) {
             httpError(404, "Device not found")
         } else {
-        	return [Device_state: device.currentValue('switch')]
+        	return [Device_state: device.currentValue('switch'), Dim: getLevelStatus(params.id)]
 	}
 }
 
 def turnOnSwitch() {
     def device = switches.find { it.id == params.id }
-    if (command){
         if (!device) {
             httpError(404, "Device not found")
         } else {
@@ -507,12 +760,10 @@ def turnOnSwitch() {
                   
             return [Device_id: params.id, result_action: "200"]
         }
-    }
 }
 
 def turnOffSwitch() {
     def device = switches.find { it.id == params.id }
-    if (command){
         if (!device) {
             httpError(404, "Device not found")
         } else {
@@ -521,22 +772,21 @@ def turnOffSwitch() {
                   
             return [Device_id: params.id, result_action: "200"]
         }
-    }
 }
 
 
 //TEMPERATURE
 def getTempSensorsStatus() {
-	
     def device = temperature_sensors.find { it.id == params.id }
     if (!device) {
             httpError(404, "Device not found")
         } else {
         	def bat = getBatteryStatus(device.id)
-        	return [Device_state: device.currentValue('temperature')] + bat
+            def scale = [Scale: location.temperatureScale]
+        	return [Device_state: device.currentValue('temperature')] + scale + bat
    }
 }
-   
+
 def getTempSensorsStatus(id) {	
     def device = temperature_sensors.find { it.id == id }
     if (!device) {
